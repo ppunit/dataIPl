90793ddea8ba5d70cdcaabc3ad5566d8ee4078e3
Puneet Purohit <puneet.purohit@mountblue.io>
2018-12-25 09:37:58 +0530

    stacked bar chart has to complete

--- server/demo.js
+++ server/demo.js
@@ -8,9 +8,9 @@ var teams={};
 var count=0,j=0;
 var currentYear=words[0].season;
 matches[currentYear]=[];
+var unique;
 
-module.exports={
-    won: function(){
+
 for(var i=0;i<words.length;i++)
 {
 currentYear=words[i].season;
@@ -23,7 +23,7 @@ currentYear=words[i].season;
     i=i-1;
     }
     else{
-        
+         unique = Object.keys(teams).filter( onlyUnique );
         matches[currentYear]=teams;
         currentYear=words[++i].season;
         matches[currentYear]=[];
@@ -36,9 +36,19 @@ currentYear=words[i].season;
 }
 matches[currentYear]=teams;
 }
-console.log(teams);
+console.log(unique);
 
-return matches;
+// var year=Object.keys(matches);
+// var team=Object.values(matches);
 
-    }
+// var teamate=[];
+// var j=0;
+function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
 }
+
+// usage example:
+
+// var unique = Object.values(team).filter( onlyUnique ); // returns ['a', 1, 2, '1']
+
+// console.log(unique);
